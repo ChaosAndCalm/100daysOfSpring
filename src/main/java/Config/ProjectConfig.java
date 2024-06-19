350067eb35fab9ae97a1d60c47f9900e0461863b
= <aditya.sheth@saavn.com>
2024-06-19 23:22:01 +0530

    Spring Configuration and Bean creation with different params

--- src/main/java/Config/ProjectConfig.java
+++ src/main/java/Config/ProjectConfig.java
@@ -4,6 +4,8 @@ import beans.MyBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.management.MXBean;
+
 @Configuration
 public class ProjectConfig {
 
@@ -12,5 +14,12 @@ public class ProjectConfig {
         MyBean b1 = new MyBean();
         b1.setText("Hello");
         return b1;
+    }
+
+    @Bean
+    public MyBean myBean2(){
+        MyBean b2 = new MyBean();
+        b2.setText("World");
+        return b2;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,8 +12,11 @@ public class Main {
     public static void main(String[] args){
         try(AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class)){
 
-            MyBean b1 = context.getBean(MyBean.class);
+            MyBean b1 = context.getBean("myBean", MyBean.class);
             System.out.println(b1.getText());
+
+            MyBean b2 = context.getBean("myBean2", MyBean.class);
+            System.out.println(b2.getText());
         }
     }
 }
